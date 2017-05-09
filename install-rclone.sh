52827c723bc6ba714d44bd0b9c4ee6a38a255d2c
Bob <bob+git@vulpin.com>
2017-05-09 02:19:08 +0000

    fix script

--- install-rclone.sh
+++ install-rclone.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+set -e
+
 # from https://rclone.org/install/
 
-wget https://downloads.rclone.org/rclone-current-linux-amd64.zip
+curl -O https://downloads.rclone.org/rclone-current-linux-amd64.zip
 unzip rclone-current-linux-amd64.zip
-pushd cd rclone-*-linux-amd64
+cd rclone-*-linux-amd64
 
 cp rclone /usr/bin/
 chown root:root /usr/bin/rclone
@@ -13,5 +15,3 @@ chmod 755 /usr/bin/rclone
 mkdir -p /usr/local/share/man/man1
 cp rclone.1 /usr/local/share/man/man1/
 mandb
-
-popd
